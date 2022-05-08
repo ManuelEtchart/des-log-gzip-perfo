4f1c445cc7a282f4cea1149be03c792883e6b2a7
Manuel Etchart <manuetchart@live.com.ar>
2022-05-08 12:59:26 -0300

    commit inicial

---
+++ carrito.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import CarritoDaoMongoDB from './src/DAOs/carritoDaoMongoDB.js';
+const carrito = express.Router();
+
+
+const carritoMonDB = new CarritoDaoMongoDB()
+
+carrito.use(express.json());
+carrito.use(express.urlencoded({extended: true}))
+
+carrito.post('', async (req,res) => {
+    try {
+        res.json(await carritoMonDB.save(
+            {
+                timestamp: Date.now(),
+                productos: []
+            }
+        ));
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+})
+
+carrito.delete('/:id', async (req,res) => {
+    try {
+        res.json(await carritoMonDB.deleteById(req.params.id))
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+})
+
+carrito.get('/:id?/productos', async (req,res) => {
+    if(req.params.id === undefined){
+        res.send(await carritoMonDB.getAll())
+    }
+    res.json(await carritoMonDB.getById(req.params.id))
+})
+
+carrito.post('/:id/productos/:id_prod', async (req,res) => {
+    try {
+        res.json(await carritoMonDB.agregarProductoEnCarrito(req.params.id, req.params.id_prod))
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+})
+
+carrito.delete('/:id/productos/:id_prod', async (req,res) => {
+    try {
+        res.json( await carritoMonDB.borrarProductoEnCarrito(req.params.id,req.params.id_prod))
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+})
+
+
+export default carrito;
---
+++ productos.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import ProductosDaoMongoDB from './src/DAOs/productosDaoMongoDB.js';
+
+const productos = express.Router();
+
+export const productoMonDB = new ProductosDaoMongoDB();
+
+productos.use(express.json());
+productos.use(express.urlencoded({extended: true}));
+
+const administrador = true;
+
+productos.get('/:id?', async (req,res) => {
+    try {
+        if (req.params.id === undefined) {
+            res.json(await productoMonDB.getAll())
+        }
+        res.json(await productoMonDB.getById(req.params.id))
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+})
+
+productos.post('', async (req,res) => {
+    try{
+        if(administrador){
+            
+            res.json(await productoMonDB.save({
+                timestamp: Date.now(),
+                nombre: req.query.nombre,
+                descripcion: req.query.descripcion,
+                codigo: req.query.codigo,
+                foto: req.query.foto,
+                precio: req.query.precio,
+                stock: req.query.stock
+            }));
+            
+        }else{
+            res.send({error: '-1', descripcion: `ruta ${req.url} metodo ${req.method} no autorizada`});
+        }
+    }catch(error){
+        console.log(error, "Hubo un error");
+    }
+});
+
+productos.put('/:id', async (req,res) => {
+    try {
+        if(administrador){
+            res.json(await productoMonDB.updateById(req.params.id, req.query));
+        }else{
+            res.send({error: '-1', descripcion: `ruta ${req.url} metodo ${req.method} no autorizada`});
+        }
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+})
+
+productos.delete('/:id', async (req,res) => {
+    try {
+        if(administrador){
+            res.json(await productoMonDB.deleteById(req.params.id))
+        }else{
+            res.send({error: '-1', descripcion: `ruta ${req.url} metodo ${req.method} no autorizada`});
+        }
+    } catch (error) {
+        console.log(error, "Hubo un error");
+    }
+    
+})
+
+export default productos;
