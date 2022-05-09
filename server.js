8f278963cfe495bf704ba3cb7841049858d23fc6
Manuel Etchart <manuetchart@live.com.ar>
2022-05-09 10:37:29 -0300

    cambios minimos

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.put('*', (req,res) => {
     logger.warn({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`})
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const server = app.listen(PORT, () => {
    logger.info(`Servidor escuchando en el puerto ${server.address().PORT}`);
